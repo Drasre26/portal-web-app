2f26641e725f1a42f912b93c8be2d3cdde2a31ae
Drasly Rafael <drafaele@miumg.edu.gt>
2022-05-05 09:26:14 -0600

    Se agrego pantalla de editar perfil del usuario
    Se agrego pantalla de editar perfil del usuario

--- src/components/Admin/Perfil/Perfil.vue
+++ src/components/Admin/Perfil/Perfil.vue
@@ -1,5 +1,33 @@
 <template>
     <div>
-        <h1>Colocar el menu Aqui de Imprimir Carnet Eventos</h1>
+        <v-row dense class="mx-6">
+            <v-col cols="6">
+                <v-card class="mx-auto my-12 pa-1">
+                    <Formulario></Formulario>
+                </v-card>
+
+                
+            </v-col>
+            <v-col cols="6">
+                <v-card class="mx-auto my-12 pa-1" max-width="200">
+                    <v-img height="200"
+                    src="https://cdn-icons-png.flaticon.com/512/3314/3314883.png"
+                    >
+                    </v-img>
+                    <v-btn class="my-2" color="deep-purple lighten-2" block dark>
+                        Mis Eventos
+                    </v-btn>
+                </v-card>
+            </v-col>
+        </v-row>
     </div>
 </template>
+<script>
+import Formulario from '@/components/Inscripcion/Formulario.vue'
+
+export default {
+    components:{
+        Formulario
+    }
+}
+</script>
--- src/components/Inscripcion/Formulario.vue
+++ src/components/Inscripcion/Formulario.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h2>Inscripción</h2>
+    <h2>{{tituloformulario}}</h2>
     <hr />
     <v-container fluid>
       <v-row>
@@ -72,9 +72,12 @@
         </v-col>
       </v-row>
       <v-row>
+        <pre>{{getUsuario}}</pre>
         <v-col>
           <v-spacer></v-spacer>
-          <v-btn color="red" dark @click="crearUsuario">Inscribrirme</v-btn>
+          <v-btn color="orange" v-if="getUsuario" dark @click="actualizarUsuario">Editar Perfil</v-btn>
+          <v-btn color="red" v-else dark @click="crearUsuario">Inscribrirme</v-btn>
+          
         </v-col>
       </v-row>
     </v-container>
@@ -86,6 +89,7 @@ import Swal from 'sweetalert2'
 
 export default {
   data: () => ({
+    tituloformulario:'Inscripcion',
     usuario:{
       nombre: "",
       apellido: "",
@@ -104,18 +108,49 @@ export default {
     },
 
     }),
+    created(){
+      this.main()
+    },
     computed:{
         urlApi(){
            return this.$store.getters.getUrlApi
+        },
+        getUsuario(){
+          return this.$store.getters.usuarioAuth.usuario
         }
     },
     methods:{
+      main(){
+        const {usuario} = this.$store.getters.usuarioAuth
+        //console.log(usuario)
+        if(usuario){
+          this.tituloformulario = 'Editar Perfil'
+          this.usuario = usuario
+        }
+      },
+      async guardar(){
+        const {usuario} = this.$store.getters.usuarioAuth
+        if(usuario){
+          this.actualizarUsuario()
+        }else{
+          this.crearUsuario()
+        }
+      },
       async crearUsuario(){
         try {
-          console.log(this.usuario)
           await axios.post(`${this.urlApi}/usuarios`,this.usuario)
           this.notificationSwal('success','Ha sido registrado exitosamente','Revise su correo electronico para activar inscripcion')
           this.usuario = Object.assign({}, this.default)
+        } catch (error) {
+          this.notificationSwal('error','Error en su registro','Intentelo mas tarde')
+          console.log(error)
+        }
+      },
+      async actualizarUsuario(){
+        try {
+         const {data} =  await axios.put(`${this.urlApi}/usuarios/${this.usuario.idusuario}`,this.usuario)
+          this.notificationSwal('success','Usuario Actualizado','El usuario se actualizo correctamente')
+          this.usuario = Object.assign({}, data)
         } catch (error) {
           this.notificationSwal('error','Error en su registro','Intentelo mas tarde')
           console.log(error)
